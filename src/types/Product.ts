14269cd9a1a83c2165f5f8768b10b63b03385dad
waiyanhtet1 <waiyanhtet1661390@gmail.com>
2024-05-01 16:46:54 +0630

    add: product type

---
+++ src/types/Product.ts
@@ -0,0 +1,12 @@
+export type Product = {
+  name: string;
+  slug: string;
+  image: string;
+  category: string;
+  brand: string;
+  price: number;
+  countInStock: number;
+  descriptioin: string;
+  rating: number;
+  numReviews: number;
+};
