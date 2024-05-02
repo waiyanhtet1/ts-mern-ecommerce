1c57b107f18d5c689ca9b4a7ab5cc2c81809e32f
waiyanhtet1 <waiyanhtet1661390@gmail.com>
2024-05-02 16:34:19 +0630

    setup: cors in index

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import dotenv from "dotenv";
 import express, { Express, Request, Response } from "express";
 import { sampleProducts } from "./utils/sampleProduct";
@@ -7,6 +8,13 @@ dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 8000;
+
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:5173"],
+  })
+);
 
 app.get("/api/products", (req: Request, res: Response) => {
   res.send(sampleProducts);
