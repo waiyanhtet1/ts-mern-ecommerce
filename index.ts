ce8956314ce6dfc4ba30f0c99f79be50b39ee3eb
waiyanhtet1 <waiyanhtet1661390@gmail.com>
2024-05-02 15:05:39 +0630

    setup: backend server

---
+++ index.ts
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+import express, { Express, Request, Response } from "express";
+import { sampleProducts } from "./utils/sampleProduct";
+
+//For env File
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 8000;
+
+app.get("/api/products", (req: Request, res: Response) => {
+  res.send(sampleProducts);
+});
+
+app.listen(port, () => {
+  console.log(`Server is Fire at http://localhost:${port}`);
+});
